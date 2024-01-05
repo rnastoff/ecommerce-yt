33edcb05b6452dfd6d968ade87c408c0c59eb683
Rob Nastoff <robnastoff@gmail.com>
2024-01-04 19:55:26 -0600

    shopping cart

---
+++ app/components/AddToBag.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useShoppingCart } from "use-shopping-cart";
+import { urlFor } from "../lib/sanity";
+
+export interface ProductCart {
+  name: string;
+  description: string;
+  price: number;
+  currency: string;
+  image: any;
+}
+
+export default function AddToBag({
+  name,
+  description,
+  price,
+  currency,
+  image,
+}: ProductCart) {
+  const { addItem, handleCartClick } = useShoppingCart();
+
+  const product = {
+    name: name,
+    description: description,
+    price: price,
+    currency: currency,
+    image: urlFor(image).url(),
+    id: "fdsa",
+  };
+
+  return (
+    <Button
+      onClick={() => {
+        addItem(product), handleCartClick();
+      }}
+    >
+      Add to Cart
+    </Button>
+  );
+}
---
+++ app/components/ShoppingCartModal.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import { useShoppingCart } from "use-shopping-cart";
+
+export default function ShoppingCartModal() {
+  const { cartCount, shouldDisplayCart, handleCartClick, cartDetails } =
+    useShoppingCart();
+
+  return (
+    <Sheet open={shouldDisplayCart} onOpenChange={() => handleCartClick()}>
+      <SheetContent className="sm:max-w-lg w-[90vw]">
+        <SheetHeader>
+          <SheetTitle>Shopping Cart</SheetTitle>
+        </SheetHeader>
+
+        <div className="h-full flex flex-col justify-between">
+          <div className="mt-8 flex-1 overflow-y-auto">
+            <ul className="-my-6 divide-y divide-gray-200">
+              {cartCount === 0 ? (
+                <h1 className="py-6">You don't have any items</h1>
+              ) : (
+                <>
+                  {Object.values(cartDetails ?? {}).map((entry) => (
+                    <li key={entry.id} className="flex py-6">
+                      <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                        <Image
+                          src={entry.image as string}
+                          alt="Product Image"
+                          width={100}
+                          height={100}
+                        />
+                      </div>
+
+                      <div className="mk-4 flex flex-1 flex-col">
+                        <div>
+                          <div className="flex justify-between text-base font-medium text-gray-900">
+                            <h3>{entry.name}</h3>
+                            <p className="mk-4">${entry.price}</p>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </>
+              )}
+            </ul>
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+// 2:17:48
